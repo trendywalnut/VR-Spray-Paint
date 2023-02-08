e9330e9c64923d01e662a7a9f4a099daf313fc53
Davie Nguyen <dvn287@utexas.edu>
2023-02-07 22:49:34 -0600

    added wall texture script

--- VArt/Assets/Wall/Scripts/Wall.cs
+++ VArt/Assets/Wall/Scripts/Wall.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
+    public Texture2D texture;   // texture of the wall
+    public Vector2 textureSize = new Vector2(2048, 2048);   // texture resolution
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // create and set the texture of the wall
+        var r = GetComponent<Renderer>();
+        texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        r.material.mainTexture = texture;
     }
 }
