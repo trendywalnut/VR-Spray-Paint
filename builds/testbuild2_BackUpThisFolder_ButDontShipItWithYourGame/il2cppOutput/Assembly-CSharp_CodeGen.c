784c038ac4a4d5780dc60728edf0a502be1540fa
trendywalnut <connorb23@utexas.edu>
2023-02-13 10:53:19 -0600

    class

---
+++ builds/testbuild2_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,117 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CharacterMovementHelper::Start()
+extern void CharacterMovementHelper_Start_m18955692205974A38B1A358AC936D5C0B68A8599 (void);
+// 0x00000002 System.Void CharacterMovementHelper::Update()
+extern void CharacterMovementHelper_Update_m897D1E408300AD2AF8DA85D7F897D5F0670AA2F1 (void);
+// 0x00000003 System.Void CharacterMovementHelper::UpdateCharacterController()
+extern void CharacterMovementHelper_UpdateCharacterController_m2755EA4C5558605C27EA1E28C312BD8D1FB01001 (void);
+// 0x00000004 System.Void CharacterMovementHelper::.ctor()
+extern void CharacterMovementHelper__ctor_m307CAC667F6618A0975782724FDD11DC9A4C12FE (void);
+// 0x00000005 System.Void Spray::Start()
+extern void Spray_Start_m76B87EB6CCE7BBA67C59D88EAAF780E91FB8AE56 (void);
+// 0x00000006 System.Void Spray::Update()
+extern void Spray_Update_mB7E87708C789042E2488EB903451AD8242F7B95A (void);
+// 0x00000007 System.Void Spray::Draw()
+extern void Spray_Draw_m7E6CB7E435E7AC59A1CE5ECF7573D433EBAAE564 (void);
+// 0x00000008 System.Void Spray::OnTriggerEnter(UnityEngine.Collider)
+extern void Spray_OnTriggerEnter_m88E72963BBEE2FC58B7C8731A1202E0683FD61E6 (void);
+// 0x00000009 System.Void Spray::.ctor()
+extern void Spray__ctor_m9836BCB215D0AB487B5AFDD67B331C3B7A858DDF (void);
+// 0x0000000A System.Void Spraycan::Start()
+extern void Spraycan_Start_mB5233DD324362297A3806A38926D73C1F1B819E1 (void);
+// 0x0000000B System.Void Spraycan::Update()
+extern void Spraycan_Update_m70B38AFBF037C8B20E3B5AD8EAC550BD73B820FA (void);
+// 0x0000000C System.Void Spraycan::Draw()
+extern void Spraycan_Draw_mB18FD27BCAB5AE788F10801B855E57D539CBE6EB (void);
+// 0x0000000D System.Void Spraycan::.ctor()
+extern void Spraycan__ctor_mBBCD9B2E4EBC90AE487A468B8A1229DDB198B3FD (void);
+// 0x0000000E System.Void Wall::Start()
+extern void Wall_Start_m29E24C2A26DAA4A52386C949FEA9D33B805AE69E (void);
+// 0x0000000F System.Void Wall::Reset()
+extern void Wall_Reset_m6DAAA34AFBB9BD98E2540C138C5B7ED4E1C247C0 (void);
+// 0x00000010 System.Void Wall::.ctor()
+extern void Wall__ctor_m04126D2E920BD90F76660E756CDC65BCFEF2711B (void);
+// 0x00000011 System.Void Unity.Template.VR.XRPlatformControllerSetup::Start()
+extern void XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF (void);
+// 0x00000012 System.Void Unity.Template.VR.XRPlatformControllerSetup::.ctor()
+extern void XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E (void);
+// 0x00000013 System.UInt32 <PrivateImplementationDetails>::ComputeStringHash(System.String)
+extern void U3CPrivateImplementationDetailsU3E_ComputeStringHash_m6EA1F233618497AEFF8902A5EDFA24C74E2F2876 (void);
+static Il2CppMethodPointer s_methodPointers[19] = 
+{
+	CharacterMovementHelper_Start_m18955692205974A38B1A358AC936D5C0B68A8599,
+	CharacterMovementHelper_Update_m897D1E408300AD2AF8DA85D7F897D5F0670AA2F1,
+	CharacterMovementHelper_UpdateCharacterController_m2755EA4C5558605C27EA1E28C312BD8D1FB01001,
+	CharacterMovementHelper__ctor_m307CAC667F6618A0975782724FDD11DC9A4C12FE,
+	Spray_Start_m76B87EB6CCE7BBA67C59D88EAAF780E91FB8AE56,
+	Spray_Update_mB7E87708C789042E2488EB903451AD8242F7B95A,
+	Spray_Draw_m7E6CB7E435E7AC59A1CE5ECF7573D433EBAAE564,
+	Spray_OnTriggerEnter_m88E72963BBEE2FC58B7C8731A1202E0683FD61E6,
+	Spray__ctor_m9836BCB215D0AB487B5AFDD67B331C3B7A858DDF,
+	Spraycan_Start_mB5233DD324362297A3806A38926D73C1F1B819E1,
+	Spraycan_Update_m70B38AFBF037C8B20E3B5AD8EAC550BD73B820FA,
+	Spraycan_Draw_mB18FD27BCAB5AE788F10801B855E57D539CBE6EB,
+	Spraycan__ctor_mBBCD9B2E4EBC90AE487A468B8A1229DDB198B3FD,
+	Wall_Start_m29E24C2A26DAA4A52386C949FEA9D33B805AE69E,
+	Wall_Reset_m6DAAA34AFBB9BD98E2540C138C5B7ED4E1C247C0,
+	Wall__ctor_m04126D2E920BD90F76660E756CDC65BCFEF2711B,
+	XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF,
+	XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E,
+	U3CPrivateImplementationDetailsU3E_ComputeStringHash_m6EA1F233618497AEFF8902A5EDFA24C74E2F2876,
+};
+static const int32_t s_InvokerIndices[19] = 
+{
+	4766,
+	4766,
+	4766,
+	4766,
+	4766,
+	4766,
+	4766,
+	3769,
+	4766,
+	4766,
+	4766,
+	4766,
+	4766,
+	4766,
+	4766,
+	4766,
+	4766,
+	4766,
+	7095,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	19,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
